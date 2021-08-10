dd8d4c44bee26b5e919c784852f878b936bde9d7
krristi427 <noreply.java8@gmail.com>
2021-08-10 12:51:17 +0200

    :tada: First Commit

---
+++ src/main/java/com/example/reactionbot/ReactionBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactionbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactionBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactionBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactionbot/ReactionBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactionbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactionBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
