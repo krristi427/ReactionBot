680d21b899995c6337bf13aca06bee2dd70f2378
krist <s6krball@uni-bonn.de>
2021-09-08 13:28:26 +0200

    :sparkles: we can now use slash commands! changed from guild to jda in order to speed up the changes. More Work is needed on displaying emojis and ofc we should start working on handling reactions ;)

--- src/main/java/com/example/reactionbot/ReactionBotApplication.java
+++ src/main/java/com/example/reactionbot/ReactionBotApplication.java
@@ -1,12 +1,15 @@
 package com.example.reactionbot;
 
 import com.example.reactionbot.adapters.MessageAdapter;
+import com.example.reactionbot.adapters.MessageRoleAdapter;
 import com.example.reactionbot.adapters.ReactionDeleteAdapter;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +18,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @Slf4j
@@ -30,6 +35,9 @@ public class ReactionBotApplication {
 
     @Autowired
     private ReactionDeleteAdapter reactionDeleteAdapter;
+
+    @Autowired
+    private MessageRoleAdapter messageRoleAdapter;
 
     public static void main(String[] args) {
         SpringApplication.run(ReactionBotApplication.class, args);
@@ -47,15 +55,24 @@ public class ReactionBotApplication {
                     .setActivity(Activity.listening("%"))
                     .addEventListeners(messageAdapter)
                     .addEventListeners(reactionDeleteAdapter)
+                    .addEventListeners(messageRoleAdapter)
                     .build();
 
-            jda.upsertCommand("arr", "assign role at reaction")
-                    .addOption(OptionType.STRING, "title", "your poll needs a title")
-                    .addOption(OptionType.STRING, "text", "meaningful and supportive text")
-                    .addOption(OptionType.STRING, "map", "provide roles in role:unicodeEmoji manner")
+            jda.awaitReady();
+
+            String serverID = environment.getProperty("GUILD-ID");
+            Guild guild = jda.getGuildById(serverID);
+
+            List<OptionData> options = new ArrayList<>(8);
+            options.add(new OptionData(OptionType.STRING, "title", "your poll needs a title").setRequired(true));
+            options.add(new OptionData(OptionType.STRING, "text", "meaningful and supportive text").setRequired(true));
+            options.add(new OptionData(OptionType.STRING, "map", "provide entries in role: emoji manner").setRequired(true));
+
+            guild.upsertCommand("arr", "assign role at reaction")
+                    .addOptions(options)
                     .queue();
 
-        } catch (LoginException e) {
+        } catch (LoginException | InterruptedException e) {
             log.error("Login failed, your token is probably invalid or not provided", e);
             e.printStackTrace();
         }
--- src/main/java/com/example/reactionbot/adapters/MessageRoleAdapter.java
+++ src/main/java/com/example/reactionbot/adapters/MessageRoleAdapter.java
@@ -1,18 +1,64 @@
 package com.example.reactionbot.adapters;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+@Component
 public class MessageRoleAdapter extends ListenerAdapter {
+
+    //TODO time the poll so that one could read stats on how it went
 
     @Override
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
 
+        List<OptionMapping> title = event.getOptionsByName("title");
+        String messageTitle = title.get(0).getAsString();
 
+        List<OptionMapping> text = event.getOptionsByName("text");
+        StringBuilder messageText = new StringBuilder(text.get(0).getAsString());
 
+        List<OptionMapping> mappings = event.getOptionsByName("map");
+
+        //to match the format of role: unicode
+        //TODO refactor this in it's own method
+        String rolesWithEmoji = mappings.get(0).getAsString();
+
+        Pattern pattern = Pattern.compile("[\\w]+:\\s[\\u0000-\\uFFFF]");
+        Matcher matcher = pattern.matcher(rolesWithEmoji);
+
+        List<String> roleAndEmoji = new ArrayList<>();
+
+        while (matcher.find()) {
+            roleAndEmoji.add(matcher.group());
+        }
+
+        for (String s : roleAndEmoji) {
+
+            String[] split = s.split(": ");
+            messageText.append("\n").append(split[0]).append(" -> ").append(split[1]);
+        }
+
+        event.replyEmbeds(
+                createMessageWithEmbeds(messageTitle, messageText.toString(), "Somebody")
+                        .build()).queue();
+    }
+
+    private EmbedBuilder createMessageWithEmbeds(String title, String description, String authorName) {
+
+        return new EmbedBuilder()
+                .setTitle(title)
+                .setDescription(description)
+                .setColor(Color.magenta)
+                .setAuthor(authorName + " asked for this");
     }
 }
