f8f303b6a75af8151cb5c71cc462d37742d08363
krist <s6krball@uni-bonn.de>
2021-09-09 11:36:41 +0200

    :sparkles: emojis now appear in the text :D

--- src/main/java/com/example/reactionbot/adapters/MessageRoleAdapter.java
+++ src/main/java/com/example/reactionbot/adapters/MessageRoleAdapter.java
@@ -8,16 +8,13 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Component
 public class MessageRoleAdapter extends ListenerAdapter {
 
-    private static final String PATTERN = "[\\w]+:\\s[\\u0000-\\uFFFF]";
+    private LinkedHashMap<String, String> roleToEmoji = new LinkedHashMap<>(8, 0.75f);
 
     @Override
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
@@ -29,20 +26,21 @@ public class MessageRoleAdapter extends ListenerAdapter {
         StringBuilder messageText = new StringBuilder(text.get(0).getAsString());
 
         List<OptionMapping> mappings = event.getOptionsByName("map");
+        String[] rolesWithEmoji = mappings.get(0).getAsString().split(" ");
 
-        String rolesWithEmoji = mappings.get(0).getAsString();
+        //+=2 bc you don't need to check the emojis
+        for (int i = 0; i < rolesWithEmoji.length; i+=2) {
 
-        List<String> roleAndEmoji = groupRoleAndEmoji(rolesWithEmoji);
-
-        for (String s : roleAndEmoji) {
-
-            String[] split = s.split(": ");
-            messageText.append("\n").append(split[0]).append(" -> ").append(split[1]);
+            //drop the last character (the ":") in the string
+            rolesWithEmoji[i] = rolesWithEmoji[i].substring(0, rolesWithEmoji[i].length() - 1);
+            roleToEmoji.put(rolesWithEmoji[i], rolesWithEmoji[i + 1]);
+            messageText.append("\n").append(rolesWithEmoji[i]).append(" -> ").append(rolesWithEmoji[i + 1]);
         }
 
         event.replyEmbeds(
                 createMessageWithEmbeds(messageTitle, messageText.toString(), "Somebody")
                         .build()).queue();
+
     }
 
     private EmbedBuilder createMessageWithEmbeds(String title, String description, String authorName) {
@@ -52,19 +50,5 @@ public class MessageRoleAdapter extends ListenerAdapter {
                 .setDescription(description)
                 .setColor(Color.magenta)
                 .setAuthor(authorName + " asked for this");
-    }
-
-    private List<String> groupRoleAndEmoji(String rolesWithEmoji) {
-
-        Pattern pattern = Pattern.compile(PATTERN);
-        Matcher matcher = pattern.matcher(rolesWithEmoji);
-
-        List<String> roleAndEmoji = new ArrayList<>();
-
-        while (matcher.find()) {
-            roleAndEmoji.add(matcher.group());
-        }
-
-        return roleAndEmoji;
     }
 }
